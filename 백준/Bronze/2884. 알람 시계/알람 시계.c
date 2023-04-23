9e61ccedd23dd68c702f9ebe4793394cca0edfb6
lkh409 <72863423+lkh409@users.noreply.github.com>
2023-04-23 21:44:02 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int h, m;
+
+    scanf("%d %d", &h, &m);
+
+    if (m >= 45) {
+        printf("%d %d", h, m - 45);
+    }
+    else if (m < 45)
+        if (h == 0)
+            printf("%d %d", 23, m + 15);
+        else
+            printf("%d %d", h - 1, m + 15);
+
+    return 0;
+
+
+}
