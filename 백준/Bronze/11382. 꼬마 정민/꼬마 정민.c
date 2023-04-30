c073731a5b6e7bbd447c6f28e3c4010392e3fb7e
lkh409 <72863423+lkh409@users.noreply.github.com>
2023-04-30 22:22:08 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    long long a,b,c;
+    
+    scanf("%lld %lld %lld", &a, &b, &c);
+    
+    long long n = a+b+c;
+    printf("%lld", n);
+
+    return 0;
+}
